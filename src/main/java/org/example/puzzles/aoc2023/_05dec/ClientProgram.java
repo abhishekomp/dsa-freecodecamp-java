6baf5448878fcaf1994770afc09457bd31b626cd
abhishekomp <abhishek2283@hotmail.com>
2023-12-06 18:01:53 +0100

    AoC Day 5 Part 2 Heap space exception for part 2

--- src/main/java/org/example/puzzles/aoc2023/_05dec/ClientProgram.java
+++ src/main/java/org/example/puzzles/aoc2023/_05dec/ClientProgram.java
@@ -5,6 +5,7 @@ public class ClientProgram {
     public static void main(String[] args) {
         //Client provides the input file (in this case almanac) and asks for the nearest location value.
         LocationCalculator locationCalculator = new LocationCalculator();
+        //long lowestLocation = locationCalculator.calculateLowestLocation("day5", "Day5Sample.txt");
         long lowestLocation = locationCalculator.calculateLowestLocation("day5", "Day5Part1Input.txt");
         System.out.println(lowestLocation); //Part 1 Done: 226172555
     }
--- src/main/java/org/example/puzzles/aoc2023/_05dec/SeedNumberFetcher.java
+++ src/main/java/org/example/puzzles/aoc2023/_05dec/SeedNumberFetcher.java
@@ -3,23 +3,58 @@ package org.example.puzzles.aoc2023._05dec;
 import org.example.puzzles.aoc2023.utils.ReadTextFile;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.LongStream;
 
 public class SeedNumberFetcher {
     public List<Long> fetchSeedNumbers(String dirName, String fileName){
         ReadTextFile readTextFile = new ReadTextFile();
         List<String> strings = readTextFile.readTextFileFromResources(dirName, fileName);
-        String seedLine = strings.get(0);
-        String seedNumbersAsStr = seedLine.split(":")[1].trim(); //"79 14 55 13"
-        Pattern pattern = Pattern.compile("(\\d+)");
-        Matcher matcher = pattern.matcher(seedNumbersAsStr);
         List<Long> seedNumbers = new ArrayList<>();
+        String seedLine = strings.get(0);   //Gets the line for seed
+        String seedNumbersAsStr = seedLine.split(":")[1].trim(); //After trimming, result = "79 14 55 13"
+        Pattern pattern = Pattern.compile("(\\d+\\s\\d+)");
+        Matcher matcher = pattern.matcher(seedNumbersAsStr);
+        List<String> seedNumbersStr = new ArrayList<>();
         while(matcher.find()){
-            //seedNumbers.add(Integer.parseInt(matcher.group(1).trim()));
-            seedNumbers.add(Long.parseLong(matcher.group(1).trim()));
+            seedNumbersStr.add(matcher.group(1));
+            //seedNumbers.add(Long.parseLong(matcher.group(1).trim()));
         }
+        System.out.println("seedNumbersStr: " + seedNumbersStr);
+        //Map<Long, Long> seedStartToRangeMap = new HashMap<>();
+        for(String seedNumberWithRange : seedNumbersStr){
+            String[] split = seedNumberWithRange.split("\\s+");
+            long seedNumberStart = Long.parseLong(split[0].trim());
+            long seedRange = Long.parseLong(split[1].trim());
+            seedNumbers.add(seedNumberStart);
+            for(long i = 1; i < seedRange; i++){
+                seedNumbers.add(seedNumbers.get(seedNumbers.size() - 1) + 1);
+            }
+            //seedStartToRangeMap.put(seedNumberStart, seedRange);
+        }
+
         return seedNumbers;
     }
+
+    /*
+    This implementation is for part 1 of the puzzle. Part 2 has different requirement for extracting the seed numbers.
+     */
+//    public List<Long> fetchSeedNumbers(String dirName, String fileName){
+//        ReadTextFile readTextFile = new ReadTextFile();
+//        List<String> strings = readTextFile.readTextFileFromResources(dirName, fileName);
+//        String seedLine = strings.get(0);
+//        String seedNumbersAsStr = seedLine.split(":")[1].trim(); //"79 14 55 13"
+//        Pattern pattern = Pattern.compile("(\\d+)");
+//        Matcher matcher = pattern.matcher(seedNumbersAsStr);
+//        List<Long> seedNumbers = new ArrayList<>();
+//        while(matcher.find()){
+//            //seedNumbers.add(Integer.parseInt(matcher.group(1).trim()));
+//            seedNumbers.add(Long.parseLong(matcher.group(1).trim()));
+//        }
+//        return seedNumbers;
+//    }
 }
