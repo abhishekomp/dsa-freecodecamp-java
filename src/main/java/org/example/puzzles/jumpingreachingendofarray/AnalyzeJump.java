cea80f72c5073b5c3fe1061a6af268d2a1f558c7
abhishekomp <abhishek2283@hotmail.com>
2023-12-18 23:36:38 +0100

    Is it possible to reach the end of the array (Jump game)

--- src/main/java/org/example/puzzles/jumpingreachingendofarray/AnalyzeJump.java
+++ src/main/java/org/example/puzzles/jumpingreachingendofarray/AnalyzeJump.java
@@ -2,8 +2,26 @@ package org.example.puzzles.jumpingreachingendofarray;
 
 public class AnalyzeJump {
 
+    /**
+     * https://leetcode.com/problems/jump-game/description/
+     * You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+     * Return true if you can reach the last index, or false otherwise.
+     * @param input input array
+     * @return true if it possible to reach the end of the array.
+     */
     public boolean canReachTheEnd(int[] input){
-        return true;
+        int standingAt = input.length - 1;
+        int preIndex = standingAt - 1;
+        while(standingAt != 0){
+            if(preIndex + input[preIndex] >= standingAt){
+                standingAt = preIndex;
+            }
+            preIndex = preIndex - 1;
+            if(preIndex == -1){
+                break;
+            }
+        }
+        return standingAt == 0;
     }
 
     public int returnInt(){
--- src/test/java/org/example/puzzles/jumpingreachingendofarray/AnalyzeJumpTest.java
+++ src/test/java/org/example/puzzles/jumpingreachingendofarray/AnalyzeJumpTest.java
@@ -16,8 +16,18 @@ class AnalyzeJumpTest {
     }
 
     @Nested
-    @DisplayName("Tests for verifying for reaching the end")
+    @DisplayName("Can you reach the end of the array, Can you reach the end of the staircase")
     class ReachEndVerifier{
+
+        @Test
+        void canReachTheEnd_true_test() {
+            //int[] inputArr = {1, 1, 2, 5};    //true
+            //int[] inputArr = {3, 0, 0, 5};  //true
+            //int[] inputArr = {2, 0, 1, 5};  //true
+            int[] inputArr = {2, 0, 0, 5};
+            assertFalse(analyzeJump.canReachTheEnd(inputArr));
+        }
+
         @Test
         void canReachTheEnd_true() {
             int[] inputArr = {2, 3, 1, 1, 4};
@@ -25,9 +35,21 @@ class AnalyzeJumpTest {
         }
 
         @Test
+        void canReachTheEnd_true_another() {
+            int[] inputArr = {1, 1, 2, 5, 2, 1, 0, 0, 1, 3};
+            assertTrue(analyzeJump.canReachTheEnd(inputArr));
+        }
+
+        @Test
         void canReachTheEnd_false() {
             int[] inputArr = {3, 2, 1, 0, 4};
-            assertTrue(analyzeJump.canReachTheEnd(inputArr));
+            assertFalse(analyzeJump.canReachTheEnd(inputArr));
+        }
+
+        @Test
+        void canReachTheEnd_false_2nd_attempt() {
+            int[] inputArr = {1, 1, 2, 4, 2, 1, 0, 0, 1, 3};
+            assertFalse(analyzeJump.canReachTheEnd(inputArr));
         }
     }
 
