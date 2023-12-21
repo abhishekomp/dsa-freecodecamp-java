d8e42a957ac58bc4e9792d7051fdb1ebaa632983
abhishekomp <abhishek2283@hotmail.com>
2023-12-21 23:53:32 +0100

    Puzzle Extract the longest sequence of at most 2 distinct elements from a String. Fruits in Basket.

--- src/main/java/org/example/puzzles/fruitsInBasket/LongestContiguousSequence.java
+++ src/main/java/org/example/puzzles/fruitsInBasket/LongestContiguousSequence.java
@@ -1,6 +1,7 @@
 package org.example.puzzles.fruitsInBasket;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class LongestContiguousSequence {
     public int[] extractLongestContiguousSeq(int[] inputArr){
@@ -28,5 +29,83 @@ public class LongestContiguousSequence {
         int[] ints = sequence.stream().filter(Objects::nonNull).mapToInt(Integer::intValue).toArray();
         return ints;
         //sum = elementCountMap.values().stream().mapToInt(i -> i).sum();
+    }
+
+    public String extractLongestContiguousStringWith2DistinctCharacter(String input){
+        char[] inputArr = input.toCharArray();
+        Map<Character, Integer> elementCountMap = new HashMap<>();
+        int leftIndex = -1;
+        int rightIndex;
+        List<Character> sequence = new LinkedList<>();
+        for(rightIndex = 0; rightIndex < inputArr.length; rightIndex++){
+            Integer orDefault = elementCountMap.getOrDefault(inputArr[rightIndex], 0);
+            elementCountMap.put(inputArr[rightIndex], orDefault + 1);
+            sequence.add(inputArr[rightIndex]);
+
+            // 2 distinct character is hard coded here.
+            while(elementCountMap.size() > 2){
+                leftIndex++;
+                int updatedValue = elementCountMap.get(inputArr[leftIndex]) - 1;
+                if(updatedValue == 0){
+                    elementCountMap.remove(inputArr[leftIndex]);
+                } else {
+                    elementCountMap.put(inputArr[leftIndex], updatedValue);
+                }
+                sequence.remove(0);
+            }
+        }
+        //Integer[] integers = sequence.toArray(Integer[]::new);
+        //String result = sequence.stream().filter(Objects::nonNull).toString();
+        String result = sequence.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        return result;
+    }
+
+    /**
+     * Returns the longest contiguous sub string with exactly N distinct characters in the sub string
+     * @param input string
+     * @param numOfChars number of distinct character needed
+     * @return sub string having exactly N number of distinct characters. (Fruit in basket puzzle)
+     */
+    public String extractLongestContiguousStringWithNDistinctCharacter(String input, int numOfChars){
+        char[] inputArr = input.toCharArray();
+        Map<Character, Integer> elementCountMap = new HashMap<>();
+        int leftIndex = 0;
+        List<Character> sequence = new LinkedList<>();  //char collector
+        List<String> listOfStrings = new ArrayList<>();
+        for (int rightIndex = 0; rightIndex < inputArr.length; rightIndex++) {
+            char currentChar = inputArr[rightIndex];
+            // Check if we can add this char to any of the baskets.
+            Integer charCount = elementCountMap.getOrDefault(currentChar, 0);
+            // Case: when we cannot add this char to one of the baskets.
+            if (charCount == 0 && elementCountMap.size() == numOfChars) {
+                //this 2 is your numOfChars method argument. we will remove the hardcoding later.
+                //save the current sequence as a string
+                listOfStrings.add((sequence.stream().map(String::valueOf).collect(Collectors.joining())));
+                while (elementCountMap.size() == numOfChars) {
+                    int updatedValue = elementCountMap.get(inputArr[leftIndex]) - 1;
+                    if (updatedValue == 0) {
+                        elementCountMap.remove(inputArr[leftIndex]);
+                    } else {
+                        elementCountMap.put(inputArr[leftIndex], updatedValue);
+                    }
+                    sequence.remove(0);
+                    leftIndex++;
+                }
+            }
+            elementCountMap.put(currentChar, charCount + 1);
+            sequence.add(currentChar);
+            if(rightIndex == inputArr.length - 1){
+                listOfStrings.add(sequence.stream().map(String::valueOf).collect(Collectors.joining()));
+            }
+        }
+        //Integer[] integers = sequence.toArray(Integer[]::new);
+        //String result = sequence.stream().filter(Objects::nonNull).toString();
+        //listOfStrings.forEach(System.out::println);
+        return listOfStrings.stream()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .collect(Collectors.toList()).get(0);
+        //System.out.println(collect.get(0));
     }
 }
--- src/test/java/org/example/puzzles/fruitsInBasket/LongestContiguousSequenceTest.java
+++ src/test/java/org/example/puzzles/fruitsInBasket/LongestContiguousSequenceTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,6 +48,15 @@ class LongestContiguousSequenceTest {
     }
 
     @Test
+    void extractLongestContiguousSeq_3a() {
+        int[] inputArr = {1, 2, 3, 2, 2, 8, 8, 6, 8};
+        int[] ints = longestContiguousSequence.extractLongestContiguousSeq(inputArr);
+        System.out.println(Arrays.toString(ints));
+        int[] expectedArr = {2, 3, 2, 2};
+        assertArrayEquals(expectedArr, ints);
+    }
+
+    @Test
     void extractLongestContiguousSeq_4() {
         int[] inputArr = {1, 2, 1, 1, 3, 4, 2, 2, 2, 2, 4};
         int[] ints = longestContiguousSequence.extractLongestContiguousSeq(inputArr);
@@ -62,4 +75,57 @@ class LongestContiguousSequenceTest {
     }
 
 
+    @Test
+    void extractLongestContiguousStringWith2DistinctCharacter() {
+        String input = "Akassa";
+        String result = longestContiguousSequence.extractLongestContiguousStringWith2DistinctCharacter(input);
+        System.out.println(result);
+        String expectedString = "assa";
+        assertEquals(expectedString, result);
+    }
+
+    @Test
+    void extractLongestContiguousStringWith2DistinctCharacter_2() {
+        String input = "YellowBanana";
+        String result = longestContiguousSequence.extractLongestContiguousStringWith2DistinctCharacter(input);
+        System.out.println(result);
+        String expectedString = "anana";
+        assertEquals(expectedString, result);
+    }
+
+    @Test
+    void extractLongestContiguousStringWith2DistinctCharacter_3() {
+        String input = "YellowBanana";
+        String result = longestContiguousSequence.extractLongestContiguousStringWithNDistinctCharacter(input, 2);
+        System.out.println(result);
+        String expectedString = "anana";
+        assertEquals(expectedString, result);
+    }
+
+    @Test
+    void extractLongestContiguousStringWith3DistinctCharacter() {
+        String input = "YellowBananapapaya";
+        String result = longestContiguousSequence.extractLongestContiguousStringWithNDistinctCharacter(input, 3);
+        System.out.println(result);
+        String expectedString = "ananapapa";
+        assertEquals(expectedString, result);
+    }
+
+    @Test
+    void extractLongestContiguousStringWith3DistinctCharacter_2() {
+        String input = "akassa";
+        String result = longestContiguousSequence.extractLongestContiguousStringWithNDistinctCharacter(input, 2);
+        System.out.println(result);
+        String expectedString = "assa";
+        assertEquals(expectedString, result);
+    }
+
+    @Test
+    void test_sort_a_list_of_strings_based_on_their_length(){
+        List<String> strings = List.of("aka", "assa");  //To do: what if this list is empty then how will you handle the longest string.
+        List<String> collect = strings.stream()
+                .sorted(Comparator.comparingInt(String::length).reversed())
+                .collect(Collectors.toList());
+        System.out.println(collect.get(0));
+    }
 }
